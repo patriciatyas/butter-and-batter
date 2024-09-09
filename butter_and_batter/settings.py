65051ae855834f01b972cc521954d84190acbc39
patriciatyas <patriciatyas11@gmail.com>
2024-09-09 12:45:35 +0700

    add url deployment PWS

--- butter_and_batter/settings.py
+++ butter_and_batter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4)y3rhcz@q4nptwn#*3e%lq(%3o_io0n%ucew4k=$bylosmi83
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "patricia-herningtyas-butterandbatter.pbp.cs.ui.ac.id"]
 
 
 # Application definition
