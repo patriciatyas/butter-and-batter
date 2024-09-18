afd141c87256ab40f6108c2d01146ae525a0a02d
patriciatyas <patriciatyas11@gmail.com>
2024-09-18 10:41:29 +0700

    change endpoint

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
-    path('create-product-entry/', create_product_entry, name='create_product_entry'),
+    path('create-product-entry', create_product_entry, name='create_product_entry'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
